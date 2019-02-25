e1a58bd9c76314c9e9945a9ba29d461329dada3e
Randy Heiland <heiland@indiana.edu>
2019-02-25 13:37:00 -0500

    fix bug if no desc attrs

--- data/xml2jupyter.py
+++ data/xml2jupyter.py
@@ -122,14 +122,8 @@ fill_xml_str= """
 
 # Now parse a configuration file (.xml) and map the user parameters into GUI widgets
 #tree = ET.parse('../config/PhysiCell_settings.xml')
-root = None
-try:
-    tree = ET.parse(config_file)
-    root = tree.getroot()
-except:
-    print("\n     ERROR  in data/xml2jupyter.py:  cannot parse ", config_file)
-    sys.exit(1)
-
+tree = ET.parse(config_file)
+root = tree.getroot()
 uep = root.find('.//user_parameters')  # find unique entry point (uep) to user params
 indent = "        "
 indent2 = "          "
@@ -147,7 +141,8 @@ box_str = "\n" + indent + "box_layout = Layout(display='flex', flex_flow='row', 
 
 # TODO: cast attributes to lower case before doing equality tests; perform more testing!
 
-param_desc_count = 0
+param_count = 0
+#param_desc_count = 0
 name_count = 0
 units_count = 0
 
@@ -171,16 +166,17 @@ for child in uep:
 #    desc_row_name = None
     desc_row_name = ''
     units_btn_name = ''
+    param_count += 1
     if 'description' in child.attrib.keys():
         describe_str = child.attrib['description']
-        param_desc_count += 1
-        desc_row_name = "desc_button" + str(param_desc_count)
-        # desc_buttons_str += indent + desc_row_name + "[ \n"
-        desc_buttons_str += indent + desc_row_name + " = " + "Button(description='" + describe_str + "', disabled=True, layout=desc_button_layout) \n"
-        if (param_desc_count % 2):
-            desc_buttons_str += indent + desc_row_name + ".style.button_color = '" + colorname1 + "'\n"
-        else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
-            desc_buttons_str += indent + desc_row_name + ".style.button_color = '" + colorname2 + "'\n"
+    else:
+        describe_str = ""
+    desc_row_name = "desc_button" + str(param_count)
+    desc_buttons_str += indent + desc_row_name + " = " + "Button(description='" + describe_str + "', disabled=True, layout=desc_button_layout) \n"
+    if (param_count % 2):
+        desc_buttons_str += indent + desc_row_name + ".style.button_color = '" + colorname1 + "'\n"
+    else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
+        desc_buttons_str += indent + desc_row_name + ".style.button_color = '" + colorname2 + "'\n"
 
     if 'units' in child.attrib.keys():
         if child.attrib['units'] != "dimensionless" and child.attrib['units'] != "none":
@@ -188,7 +184,7 @@ for child in uep:
             units_count += 1
             units_btn_name = "units_button" + str(units_count)
             units_buttons_str += indent + units_btn_name + " = " + "Button(description='" + child.attrib['units'] + "', disabled=True, layout=units_button_layout) \n"
-            if (param_desc_count % 2):
+            if (param_count % 2):
                 units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname1 + "'\n"
             else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
                 units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname2 + "'\n"
@@ -196,7 +192,7 @@ for child in uep:
             units_count += 1
             units_btn_name = "units_button" + str(units_count)
             units_buttons_str += indent + units_btn_name + " = " + "Button(description='" +  "', disabled=True, layout=units_button_layout) \n"
-            if (param_desc_count % 2):
+            if (param_count % 2):
                 units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname1 + "'\n"
             else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
                 units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname2 + "'\n"
@@ -204,7 +200,7 @@ for child in uep:
         units_count += 1
         units_btn_name = "units_button" + str(units_count)
         units_buttons_str += indent + units_btn_name + " = " + "Button(description='" +  "', disabled=True, layout=units_button_layout) \n"
-        if (param_desc_count % 2):
+        if (param_count % 2):
             units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname1 + "'\n"
         else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
             units_buttons_str += indent + units_btn_name + ".style.button_color = '" + colorname2 + "'\n"
@@ -218,18 +214,13 @@ for child in uep:
             print("    *** Error - Invalid type: " + child.attrib['type'])
             sys.exit(1)
         else:    
-            # self.default_cell_speed = FloatText(
-            #   description='default_cell_speed',
-            #   step=0.02,
-            #   style=style, layout=layout)
             param_name_button = "param_name" + str(name_count)
             user_tab_header += "\n" + indent + param_name_button + " = " + "Button(description='" + child.tag + "', disabled=True, layout=name_button_layout)\n"
-            if (param_desc_count % 2):
+            if (param_count % 2):
                 user_tab_header += indent + param_name_button + ".style.button_color = '" + colorname1 + "'\n"
             else:  # rf.  https://www.w3schools.com/colors/colors_names.asp
                 user_tab_header += indent + param_name_button + ".style.button_color = '" + colorname2 + "'\n"
             user_tab_header += "\n" + indent + full_name + " = " + widgets[child.attrib['type']] + "(\n"
-#            user_tab_header += indent2 + "description='" + child.tag + "',\n"
 
             # Try to calculate and provide a "good" delta step (for the tiny "up/down" arrows on a numeric widget)
             if child.attrib['type'] == "double":
@@ -260,6 +251,15 @@ for child in uep:
 
             # Booleans
             elif child.attrib['type'] == "bool":
+                if (child.text.lower() == "true"):
+                    child.text = "True"
+                elif (child.text.lower() == "false"):
+                    child.text = "False"
+                else:
+                    print(" --- ERROR: bool must be True or False, not ", child.text)
+                    sys.exit(1)
+
+                print('bool: ',child.text)
                 user_tab_header += indent2 + "value=" + child.text + ",\n"
             
             # Strings
@@ -268,20 +268,19 @@ for child in uep:
 
 
             # Finally, append the info at the end of this widget
-#            user_tab_header += indent2 + "style=style, layout=layout)\n"
             user_tab_header += indent2 + "style=style, layout=widget_layout)\n"
 
-            row_name = "row" + str(param_desc_count)
-#                row_str += indent +  row_name + " = [" + full_name + ", Label('" + units_str + "' , layout=Layout(flex='1 1 auto', width='auto')), " + desc_row_name + "] \n"
+            row_name = "row" + str(param_count)
             row_str += indent +  row_name + " = [" + param_name_button + ", " + full_name + ", " + units_btn_name + ", " + desc_row_name + "] \n"
-#                row_str += indent +  row_name + " = [" + full_name + ", " + ", " +  "] \n"
-            box_name = "box" + str(param_desc_count)
+            box_name = "box" + str(param_count)
             box_str += indent + box_name + " = Box(children=" + row_name + ", layout=box_layout)\n"
-#        box1 = Box(children=row1, layout=box_layout)\n"
             vbox_str += indent2 + box_name + ",\n"
 
             # float, int, bool
-            fill_gui_str += indent + full_name + ".value = " + type_cast[child.attrib['type']] + "(uep.find('.//" + child.tag + "').text)\n"
+            if (type_cast[child.attrib['type']] == "bool"):
+                fill_gui_str += indent + full_name + ".value = ('true' == (uep.find('.//" + child.tag + "').text.lower()) )\n"
+            else:
+                fill_gui_str += indent + full_name + ".value = " + type_cast[child.attrib['type']] + "(uep.find('.//" + child.tag + "').text)\n"
 
             fill_xml_str += indent + "uep.find('.//" + child.tag + "').text = str("+ full_name + ".value)\n"
 
@@ -301,9 +300,7 @@ print("(or, if you already have a previous GUI running and want to see new param
 print("run the Jupyter menu item:  Kernel -> Restart & Run All)")
 print()
 fp= open(user_tab_file, 'w')
-#fp.write("\n" + indent + "param_button_layout={'width':'400px'} \n")
 fp.write(user_tab_header)
-#fp.write(param_desc_buttons_str)
 fp.write(units_buttons_str)
 fp.write(desc_buttons_str)
 fp.write(row_str)
